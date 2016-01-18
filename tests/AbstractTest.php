6626ed95a4dfcd76029eb68c9240610d355200bc
James Johnson <james.johnson@excellentingenuity.com>
2016-01-18 18:32:11 -0500

    starting to add generic tests

--- tests/AbstractTest.php
+++ tests/AbstractTest.php
@@ -2,12 +2,12 @@
 
 namespace ChainLink\PHP\Tests;
 
-/**
- * User: James Johnson
- * Date: 1/18/16
- * Time: 5:30 PM
- */
+
 class AbstractTest extends \PHPUnit_Framework_TestCase
 {
-
+    protected $testSubject;
+    public function __construct ($object)
+    {
+        $this->testSubject = $object;
+    }
 }
